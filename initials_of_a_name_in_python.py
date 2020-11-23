c9bca5f36d8b451adceb49f375fc86ae411419ab
ALL TECHZ <64519974+AllTechz@users.noreply.github.com>
2020-11-23 09:30:36 +0530

    Create initials_of_a_name_in_python.py

---
+++ initials_of_a_name_in_python.py
@@ -0,0 +1,15 @@
+Raw_name = input("\nEnter Full Name : ") 
+name = Raw_name.strip()
+bname  = name.split(' ')
+lenofstr = len(bname)
+fname = bname[0]
+leofstrstr = str(lenofstr)
+vlname = int(lenofstr) - 1
+lname = bname[int(vlname)]
+print("\nHi,"+bname[0])
+print("\nHi,",end="")
+for i in bname:
+    if i == lname:
+        break
+    print(i[0].upper()+".",end="")
+print(lname+"\n")
